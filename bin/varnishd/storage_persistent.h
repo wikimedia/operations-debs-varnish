b0a3abd8a3655becac67e62c8536e2e548c6a54d
Martin Blix Grydeland <martin@varnish-software.com>
2012-05-25 11:42:16 +0200

    Add smp_copy_sign() and smp_reapply_sign() utility functions
    smp_copy_sign() will copy the sign data of one sign onto another.
    
    smp_reapply_sign() will resign the entire length of a signed area.

--- bin/varnishd/storage_persistent.h
+++ bin/varnishd/storage_persistent.h
@@ -188,8 +188,10 @@ void smp_def_sign(const struct smp_sc *sc, struct smp_signctx *ctx,
     uint64_t off, const char *id);
 int smp_chk_sign(struct smp_signctx *ctx);
 void smp_append_sign(struct smp_signctx *ctx, const void *ptr, uint32_t len);
+void smp_reapply_sign(struct smp_signctx *ctx, uint32_t len);
 void smp_reset_sign(struct smp_signctx *ctx);
 void smp_sync_sign(const struct smp_signctx *ctx);
+void smp_copy_sign(struct smp_signctx *dst, const struct smp_signctx *src);
 void smp_newsilo(struct smp_sc *sc);
 int smp_valid_silo(struct smp_sc *sc);
 
