f659461a2a3a5e78a23159f2511b654b3655eb5e
Martin Blix Grydeland <martin@varnish-software.com>
2012-05-25 11:42:16 +0200

    Add smp_copy_sign() and smp_reapply_sign() utility functions
    smp_copy_sign() will copy the sign data of one sign onto another.
    
    smp_reapply_sign() will resign the entire length of a signed area.

--- bin/varnishd/storage_persistent_subr.c
+++ bin/varnishd/storage_persistent_subr.c
@@ -129,6 +129,20 @@ XXXAZ(smp_chk_sign(ctx));
 }
 
 /*--------------------------------------------------------------------
+ * Reapply the sign from start to len bytes
+ */
+
+void
+smp_reapply_sign(struct smp_signctx *ctx, uint32_t len)
+{
+	ctx->ss->length = 0;
+	SHA256_Init(&ctx->ctx);
+	SHA256_Update(&ctx->ctx, ctx->ss,
+	    offsetof(struct smp_sign, length));
+	smp_append_sign(ctx, SIGN_DATA(ctx), len);
+}
+
+/*--------------------------------------------------------------------
  * Reset a signature to empty, prepare for appending.
  */
 
@@ -160,6 +174,18 @@ smp_sync_sign(const struct smp_signctx *ctx)
 	if (i && 0)
 		fprintf(stderr, "SyncSign(%p %s) = %d %s\n",
 		    ctx->ss, ctx->id, i, strerror(errno));
+}
+
+/*--------------------------------------------------------------------
+ * Reset dst sign and append the data of src to it
+ */
+
+void
+smp_copy_sign(struct smp_signctx *dst, const struct smp_signctx *src)
+{
+	smp_reset_sign(dst);
+	memcpy(SIGN_DATA(dst), SIGN_DATA(src), src->ss->length);
+	smp_append_sign(dst, SIGN_DATA(dst), src->ss->length);
 }
 
 /*--------------------------------------------------------------------
