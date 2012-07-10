varnishcache/varnish-cache

Req.hash_always_miss now implies req.hash_ignore_busy.
Fixes a case where we might get a cache hit even though hash_always_miss is set.

Fixes: #1073

bin/varnishd/cache/cache_hash.c
@@ -332,7 +332,7 @@ HSH_Lookup(struct sess *sp)
 
 		if (oc->flags & OC_F_BUSY || oc->busyobj != NULL) {
 			CHECK_OBJ_ORNULL(oc->busyobj, BUSYOBJ_MAGIC);
-			if (req->hash_ignore_busy)
+			if (req->hash_ignore_busy || req->hash_always_miss)
 				continue;
 
 			if (oc->busyobj != NULL &&

wikimedia/operations-debs-varnish

bin/varnishd/cache_hash.c
@@ -352,7 +352,7 @@ HSH_Lookup(struct sess *sp, struct objhead **poh)
 
 		if (oc->flags & OC_F_BUSY) {
 			CHECK_OBJ_NOTNULL(oc->busyobj, BUSYOBJ_MAGIC);
-			if (sp->hash_ignore_busy)
+			if (sp->hash_ignore_busy || sp->hash_always_miss)
 				continue;
 
 			if (oc->busyobj->vary != NULL &&