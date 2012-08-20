robj/varnish-cache-streaming

Add background fetch and multiple client streaming support
Add parameter stream_maxchunksize that limits the storage chunks
allocated when streaming. This creates interruption points where the
receiving clients are notified of new data

Add parameter stream_tokens controlling the default number of tokens
available when new stream data arrives from backend.

Add counter values for use with streaming

Token strategy: Any thread hitting end of data will be marked as a
fast writer. It will then have to get a token before calling
RES_StreamWrite, thus n_tokens limits the number of thundering threads
run whenever new data arrives. The fast writer flag is cleared on each
pass through the loop, and reset if it hits the end of data and waits.

Expose the number of tokens as VRT functions accessible through VCL in
vcl_fetch. This allows the rate throttling to be tuned per connection.

Make conditional delivery work while streaming

Rename parameter default_tokens to stream_tokens

Update streaming documentation

Update transition graph

Make r00979.vtc compatible with threaded streaming.

Add author in files with more than trivial changes

Add WRK_QueueFirst() that will schedule a work request first, and not
take queue lengths into account. For already commited to work loads.

Add WRK_QueueSessionFirst() that will queue a session using WRK_QueueFirst().

Add SES_NewNonVCA() for getting recycled sessions from the list not
owned by the VCA

Conflicts:

	bin/varnishd/cache_center.c

bin/varnishd/cache_pool.c
@@ -286,6 +286,53 @@ WRK_Queue(struct workreq *wrq)
 	return (0);
 }
 
+/*--------------------------------------------------------------------
+ * Queue a workrequest to be scheduled first. Will always queue the 
+ * request, bypassing the max queue length. This is for work loads we 
+ * have already committed to serving
+ *
+ * Return zero
+ */
+
+static int
+WRK_QueueFirst(struct workreq *wrq)
+{
+	struct worker *w;
+	struct wq *qp;
+	static unsigned nq = 0;
+	unsigned onq;
+
+	/*
+	 * Select which pool we issue to
+	 * XXX: better alg ?
+	 * XXX: per CPU ?
+	 */
+	onq = nq + 1;
+	if (onq >= nwq)
+		onq = 0;
+	qp = wq[onq];
+	nq = onq;
+
+	Lck_Lock(&qp->mtx);
+
+	/* If there are idle threads, we tickle the first one into action */
+	w = VTAILQ_FIRST(&qp->idle);
+	if (w != NULL) {
+		VTAILQ_REMOVE(&qp->idle, w, list);
+		Lck_Unlock(&qp->mtx);
+		w->wrq = wrq;
+		AZ(pthread_cond_signal(&w->cond));
+		return (0);
+	}
+
+	VTAILQ_INSERT_HEAD(&qp->queue, wrq, list);
+	qp->nqueue++;
+	qp->lqueue++;
+	Lck_Unlock(&qp->mtx);
+	AZ(pthread_cond_signal(&herder_cond));
+	return (0);
+}
+
 /*--------------------------------------------------------------------*/
 
 static void
@@ -331,6 +378,24 @@ WRK_QueueSession(struct sess *sp)
 		VCL_Rel(&sp->vcl);
 	}
 	SES_Delete(sp);
+	return (1);
+}
+
+/*--------------------------------------------------------------------
+ * Queue high priority session (one that we have already commited to 
+ * serving). Will not fail to Queue.
+ *
+ * Always returns 1
+ */
+
+int
+WRK_QueueSessionFirst(struct sess *sp)
+{
+	CHECK_OBJ_NOTNULL(sp, SESS_MAGIC);
+	AZ(sp->wrk);
+	sp->workreq.func = wrk_do_cnt_sess;
+	sp->workreq.priv = sp;
+	AZ(WRK_QueueFirst(&sp->workreq));
 	return (1);
 }
 

wikimedia/operations-debs-varnish

	bin/varnishd/cache.h
	bin/varnishd/cache_hash.c
	bin/varnishd/heritage.h
	bin/varnishd/mgt_param.c

bin/varnishd/cache_pool.c
@@ -290,6 +290,53 @@ WRK_Queue(struct sess *sp)
 	return (0);
 }
 
+/*--------------------------------------------------------------------
+ * Queue a workrequest to be scheduled first. Will always queue the 
+ * request, bypassing the max queue length. This is for work loads we 
+ * have already committed to serving
+ *
+ * Return zero
+ */
+
+static int
+WRK_QueueFirst(struct sess *sp)
+{
+	struct worker *w;
+	struct wq *qp;
+	static unsigned nq = 0;
+	unsigned onq;
+
+	/*
+	 * Select which pool we issue to
+	 * XXX: better alg ?
+	 * XXX: per CPU ?
+	 */
+	onq = nq + 1;
+	if (onq >= nwq)
+		onq = 0;
+	qp = wq[onq];
+	nq = onq;
+
+	Lck_Lock(&qp->mtx);
+
+	/* If there are idle threads, we tickle the first one into action */
+	w = VTAILQ_FIRST(&qp->idle);
+	if (w != NULL) {
+		VTAILQ_REMOVE(&qp->idle, w, list);
+		Lck_Unlock(&qp->mtx);
+		w->sp = sp;
+		AZ(pthread_cond_signal(&w->cond));
+		return (0);
+	}
+
+	VTAILQ_INSERT_HEAD(&qp->queue, sp, poollist);
+	qp->nqueue++;
+	qp->lqueue++;
+	Lck_Unlock(&qp->mtx);
+	AZ(pthread_cond_signal(&herder_cond));
+	return (0);
+}
+
 /*--------------------------------------------------------------------*/
 
 int
@@ -316,6 +363,22 @@ WRK_QueueSession(struct sess *sp)
 		VCL_Rel(&sp->vcl);
 	}
 	SES_Delete(sp);
+	return (1);
+}
+
+/*--------------------------------------------------------------------
+ * Queue high priority session (one that we have already commited to 
+ * serving). Will not fail to Queue.
+ *
+ * Always returns 1
+ */
+
+int
+WRK_QueueSessionFirst(struct sess *sp)
+{
+	CHECK_OBJ_NOTNULL(sp, SESS_MAGIC);
+	AZ(sp->wrk);
+	AZ(WRK_QueueFirst(sp));
 	return (1);
 }
 