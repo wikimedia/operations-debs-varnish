togge/varnish-cache

Generalize the ban reporting to the stevedores using their API. This way any stevedore interested in new bans can request to be notified (not just the persistent).

bin/varnishd/storage/stevedore.c
@@ -442,6 +442,15 @@ STV_close(void)
 		stv->close(stv);
 }
 
+void
+STV_NewBan(const struct ban *ban)
+{
+	struct stevedore *stv;
+
+	VTAILQ_FOREACH(stv, &stv_stevedores, list)
+		if (stv->newban != NULL)
+			stv->newban(stv, ban);
+}
 
 /*--------------------------------------------------------------------
  * VRT functions for stevedores

wikimedia/operations-debs-varnish

bin/varnishd/stevedore.c
@@ -425,6 +425,16 @@ STV_close(void)
 		stv->close(stv);
 }
 
+void
+STV_NewBan(const struct ban *ban)
+{
+	struct stevedore *stv;
+
+	VTAILQ_FOREACH(stv, &stevedores, list)
+		if (stv->newban != NULL)
+			stv->newban(stv, ban);
+}
+
 /*--------------------------------------------------------------------
  * Parse a stevedore argument on the form:
  *	[ name '=' ] strategy [ ',' arg ] *